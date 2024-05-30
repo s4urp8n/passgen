114c52ae037f9789cdf12c661062c3b67866712f
tarasov <s4urp8n@yandex.ru>
2024-05-30 23:53:01 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import classes from './App.module.css';
+import classes from './App.module.scss';
 
 function App() {
     return (
         <div className={classes.App}>
-            <header className={classes.AppHeader}>
-                <img src={logo}
-                     className={classes.AppLogo}
-                     alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className={classes.AppLink}
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda atque corporis doloribus ducimus eius esse harum id iure iusto, molestias natus necessitatibus nostrum nulla quod, repellat repudiandae voluptates voluptatibus?
         </div>
     );
 }
